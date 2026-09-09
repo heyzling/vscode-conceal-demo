bab9c971541b791282bd2064297016fe2dead93c
heyzling <heyzling@outlook.com>
2026-09-09 17:44:03 +0300

    delete mask demo; re-record 01-tags

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@
 
 import * as vscode from "vscode";
 import * as tags from "./cases/01-tags/tags";
-import * as mask from "./cases/04-mask/mask";
 import * as fold from "./cases/07-fold/fold";
 import * as recorder from "./recorder";
 import * as toggle from "./toggle";
@@ -13,7 +12,6 @@ import * as toggle from "./toggle";
 export function activate(context: vscode.ExtensionContext): void {
   toggle.activate(context);
   tags.activate(context);
-  mask.activate(context);
   fold.activate(context);
   // Dev tooling: plays the scenes behind the README's GIFs, and does nothing otherwise.
   recorder.activate();
